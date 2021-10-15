aa3ee0a3db2236d3b90e409941c858e8ed6c11cc
kiwkiw <70262096+rickyzatnika@users.noreply.github.com>
2021-10-15 17:42:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let menuBtn = document.getElementById('menuBtn');
+let sidenav = document.getElementById('sidenav');
+
+sidenav.style.right = '-250px';
+menuBtn.onclick = function () {
+  if (sidenav.style.right == '-250px') {
+    sidenav.style.right = '0';
+  } else {
+    sidenav.style.right = '-250px';
+  }
+};
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1500,
+  speedAsDuration: true,
+});
